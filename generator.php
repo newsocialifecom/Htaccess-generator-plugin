9600b5d92d9c7212c0427eba2b385e8bdf6c4e15
Danny Morabito <staff@newsocialife.com>
2013-05-25 17:10:51 +0200

    Create generator.php
    This file is the main controller, it loads the necessary files and writes .htaccess or .htpasswd

---
+++ generator.php
@@ -0,0 +1,43 @@
+<?php
+include "../../common.php";
+if(!file_exists($_GET["projectpath"]."/.htaccess"))
+    file_put_contents($_GET["projectpath"]."/.htaccess", "");
+function writecode($file = "htaccess", $toWrite = "") {
+    if(!in_array($file, array("htaccess", "htpasswd")))
+        $file = "htaccess";
+    $code = file_get_contents($_GET["projectpath"]."/.{$file}");
+    $code .= $toWrite.PHP_EOL;
+    unlink($_GET["projectpath"]."/.{$file}");
+    file_put_contents($_GET["projectpath"]."/.{$file}", $code);
+}
+switch($_GET["action"]) {
+    case "view":
+        echo file_get_contents("view.html");
+    break;
+    case "delete":
+        unlink($_GET["projectpath"]."/.htaccess");
+        file_put_contents($_GET["projectpath"]."/.htaccess", "");
+        echo '<span style="color: red">.htaccess deleted succefully</span>';
+        echo file_get_contents("view.html");
+    break;
+    case "errorpages":
+        echo file_get_contents("errorpages.html");
+    break;
+    case "fileindex":
+        echo file_get_contents("fileindex.html");
+    break;
+    case "homepage":
+        echo file_get_contents("homepage.html");
+    break;
+    case "protector":
+        echo file_get_contents("protector.html");
+    break;
+    case "write":
+        writecode(isset($_GET["file"]) ? $_GET["file"] : "htaccess", $_GET["code"]);
+    break;
+    case "password":
+        $username = $_POST["username"];
+        $password = crypt($_POST["password"], base64_encode($_POST["password"]));
+        writecode("htpasswd", "{$username}:{$password}");
+    break;
+}
